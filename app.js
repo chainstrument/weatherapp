21cbea4edd2f22faf86147c5d2a35461f285331b
Chainstrument <minitutoriel@gmail.com>
2024-09-15 19:27:25 +0200

    reglage url api

---
+++ app.js
@@ -0,0 +1,35 @@
+ let valueSearch = document.getElementById('valueSearch'); 
+
+ let city = document.getElementById('city'); 
+
+ let temperature = document.getElementById('temperature');
+ let description = document.querySelector('.description');
+ let clouds = document.getElementById('clouds');
+ let humidity = document.getElementById('humidity');
+ let pressure = document.getElementById('pressure');
+
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(valueSearch.value != '')
+    {
+        searchWeather();
+    }
+})
+
+let id = 'b47e368a40b89e4cdc426535743f6607';
+let url = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=' + id; 
+
+//console.log(url);
+
+//https://api.openweathermap.org/data/2.5/weather?q=lille&appid=b47e368a40b89e4cdc426535743f6607&units=metric
+
+const searchWeather = () => {
+    fetch(url + '&q=' + valueSearch.value)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+
+}
